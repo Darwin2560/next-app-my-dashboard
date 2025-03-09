11bb1f9a70a537e6027bb2aa7735690c08babf0d
jrdarwin <junior.ruiz.perez00@gmail.com>
2025-03-09 12:44:26 +0100

    feat: shopping cart

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,18 @@
+import { CartCounter } from "@/app/shopping-cart/components/CartCounter";
+
+
+export const metadata = {
+    title: 'Shopping Cart',
+    description: 'A simple counter'
+}
+
 export default function CounterPage() {
     return (
-        <h1>
-            Counter Page
-        </h1>
+        <div className={`flex flex-col items-center justify-center w-full h-full`}>
+            <span className={``}>Products in the cart</span>
+
+            <CartCounter />
+
+        </div>
     )
 }
