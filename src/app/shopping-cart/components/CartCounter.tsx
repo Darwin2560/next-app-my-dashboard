d84ebfd415d65e731e0143fa7b1293fe2942b900
jrdarwin <junior.ruiz.perez00@gmail.com>
2025-03-09 21:33:16 +0100

    feat: the position of the elementos has been changed

--- src/app/shopping-cart/components/CartCounter.tsx
+++ src/app/shopping-cart/components/CartCounter.tsx
@@ -11,15 +11,15 @@ export const CartCounter = () => {
             <span className={`text-9xl`}>{ count }</span>
             <div className={`flex`}>
                 <button
-                    onClick={() => setCount(count + 1)}
-                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2 cursor-pointer">
-                    +1
-                </button>
-
-                <button
                     onClick={() => setCount(count - 1)}
                     className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2 cursor-pointer">
                     -1
+                </button>
+
+                <button
+                    onClick={() => setCount(count + 1)}
+                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2 cursor-pointer">
+                    +1
                 </button>
             </div>
         </>
